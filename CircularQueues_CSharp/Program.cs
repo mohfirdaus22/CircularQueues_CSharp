7b56de6c057acfa35af074f615dab7b846d8705b
mohfirdaus22 <thepashter6@gmail.com>
2022-12-16 17:36:04 +0700

    public void display

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -59,6 +59,19 @@ namespace CircularQueues_CSharp
                     FRONT = FRONT + 1;
             }
         }
+        public void display()
+        {
+            int FRONT_position = FRONT;
+            int REAR_position = REAR;
+            /* Checks if the queue is empty. */
+            if (FRONT == -1)
+            {
+                Console.WriteLine("Queue is empty\n");
+                return;
+            }
+
+        }
+
 
 
 
