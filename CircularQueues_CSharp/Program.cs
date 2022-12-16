7bbbc1b19ab9c725f7a217afc091efe29dfa8655
mohfirdaus22 <thepashter6@gmail.com>
2022-12-16 17:31:09 +0700

    Add public void insert

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -8,8 +8,20 @@ namespace CircularQueues_CSharp
         int[] queue_array = new int[5];
         public Queues()
         {
-
+            /*Initializing the values of the variabels REAR and FRONT to -1 to indicate that the queue is initially empty. */
+            FRONT = -1;
+            REAR = -1;
         }
 
+        public void insert(int element)
+        {
+            /* The following statement checks whether the queue is empty. If the queue is empty, then the value of the REAR and FRONT variable is set to 0. */
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+
+        }
     }
 }
