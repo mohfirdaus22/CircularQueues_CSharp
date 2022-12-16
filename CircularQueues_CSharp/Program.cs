c467afdd2feaf8aadc5ebab9d9a9677fe7e50b4c
mohfirdaus22 <thepashter6@gmail.com>
2022-12-16 17:31:49 +0700

    Add else

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -21,6 +21,15 @@ namespace CircularQueues_CSharp
                 FRONT = 0;
                 REAR = 0;
             }
+            else
+            {
+                /* If REAR is at the last position of the array, then the value of REAR is set to 0 that corresponds to the first position of the array. */
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    /*If REAR is not at the last position, then its value is incremend by one.  */
+                    REAR = REAR + 1;
+            }
 
         }
     }
