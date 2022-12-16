c34e04283f767f7cc1690bec86643c4d91e88a80
mohfirdaus22 <thepashter6@gmail.com>
2022-12-16 17:46:23 +0700

    Add try

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -104,7 +104,19 @@ namespace CircularQueues_CSharp
                 char ch;
                 while (true)
                 {
+                    try
+                    {
+                        Console.WriteLine("Menu");
+                        Console.WriteLine("1. Implement Insert Operation");
+                        Console.WriteLine("2. Implement Delete Operation");
+                        Console.WriteLine("3. Display Values");
+                        Console.WriteLine("4. Exit");
+                        Console.WriteLine("\nEnter Your Choice (1-4): ");
+                        ch = Convert.ToChar(Console.ReadLine());
+                        Console.WriteLine();
+                        switch (ch)
 
+                    }
                 }
 
             }
